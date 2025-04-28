0922d9c235baabb0f46a1d648dab522292a13585
marah <marah.daher@l-one.de>
2025-04-28 09:29:49 +0300

    Delete files

--- t/unzip.php
+++
@@ -1,9 +0,0 @@
-<?php
-$zip = new ZipArchive;
-if ($zip->open('vendor.zip') === TRUE) {
-    $zip->extractTo('./'); // فك الضغط في نفس المجلد
-    $zip->close();
-    echo '✅ Unzipped successfully.';
-} else {
-    echo '❌ Failed to unzip.';
-}
